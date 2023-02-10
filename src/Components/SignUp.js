dc8048bd868bd87b929549244665219384b99097
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-02-10 10:30:16 +0530

    tailwind to moon....

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, FormControl, Input, InputLabel, FormHelperText, Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+// import { Box, FormControl, Input, InputLabel, FormHelperText, Button } from '@mui/material';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from "./Firebase";
 
@@ -103,11 +103,13 @@ function Form() {
             createUserWithEmailAndPassword(auth, email, pw)
                 .then((userCredential) => {
                     const user = userCredential.user;
+                    console.log(user);
                     nav("/signin");
                 })
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
+                    console.log(errorCode, errorMessage);
                     alert("Something went wrong. Please try again!");
                 });
         }
@@ -115,52 +117,88 @@ function Form() {
     }
 
     return (
-        <Box className='box' sx={{
-            backgroundColor: '#f06292',
-            width: 450,
-            height: 520,
-            borderRadius: 25
-        }}>
-            <div className='box-conts'>
+        // <Box className='box' sx={{
+        //     backgroundColor: '#f06292',
+        //     width: 450,
+        //     height: 520,
+        //     borderRadius: 25
+        // }}>
+        //     <div className='box-conts'>
 
-                <center><h2>SIGN UP</h2></center>
+        //         <center><h2>SIGN UP</h2></center>
 
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="uname">Email</InputLabel>
-                    <Input
-                        id="uname"
+        //         <FormControl variant="standard">
+        //             <InputLabel htmlFor="uname">Email</InputLabel>
+        //             <Input
+        //                 id="uname"
+        //                 onChange={handleUn}
+        //             />
+        //         </FormControl>
+        //         <br /><br />
+
+        //         <FormControl variant="standard">
+        //             <InputLabel htmlFor="pw">Password</InputLabel>
+        //             <Input
+        //                 type="password"
+        //                 id="pw"
+        //                 onChange={handlePw}
+        //                 className={{}}
+        //             />
+        //             <FormHelperText id="pwl-text" sx={{ color: "red", }}>{pwlength}</FormHelperText>
+        //             <FormHelperText id={pwc} sx={{ color: { pwc } }}>{pwtxt}</FormHelperText>
+        //         </FormControl>
+        //         <br /><br />
+
+        //         <FormControl variant="standard">
+        //             <InputLabel htmlFor="cpw">Confirm Password</InputLabel>
+        //             <Input
+        //                 type="password"
+        //                 id="cpw"
+        //                 onChange={handleCpw}
+        //             />
+        //             <FormHelperText id={cpwc} sx={{ color: "red" }}>{cpwtxt}</FormHelperText>
+        //         </FormControl>
+        //         <br /><br />
+
+        //         <center><Button variant="oultined" onClick={handleSubmit}>SignUp</Button></center>
+        //     </div>
+        // </Box>
+
+        // TODO: rewrite above code in Tailwind CSS with plugins like @tailwindcss/forms and @tailwindcss/typography
+        <div className="bg-pink-200 h-screen flex justify-center items-center">
+            <div className="bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-bold text-gray-800 mt-5">Sign Up</h1>
+                <div className="flex flex-col justify-center items-center mt-8">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-72 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-pink-500 mt-4"
                         onChange={handleUn}
                     />
-                </FormControl>
-                <br /><br />
-
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="pw">Password</InputLabel>
-                    <Input
+                    <input
                         type="password"
-                        id="pw"
+                        placeholder="Password"
+                        className="w-72 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-pink-500 mt-4"
                         onChange={handlePw}
-                        className={{}}
                     />
-                    <FormHelperText id="pwl-text" sx={{ color: "red", }}>{pwlength}</FormHelperText>
-                    <FormHelperText id={pwc} sx={{ color: { pwc } }}>{pwtxt}</FormHelperText>
-                </FormControl>
-                <br /><br />
-
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="cpw">Confirm Password</InputLabel>
-                    <Input
+                    <input
                         type="password"
-                        id="cpw"
+                        placeholder="Confirm Password"
+                        className="w-72 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-pink-500 mt-4"
                         onChange={handleCpw}
                     />
-                    <FormHelperText id={cpwc} sx={{ color: "red" }}>{cpwtxt}</FormHelperText>
-                </FormControl>
-                <br /><br />
-
-                <center><Button variant="oultined" onClick={handleSubmit}>SignUp</Button></center>
+                    <button
+                        className="bg-pink-500 w-72 h-10 rounded-lg text-white mt-4"
+                        onClick={handleSubmit}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div className="flex flex-col justify-center items-center h-full">
+                    <p className="text-sm text-gray-500 mt-2">Already have an account? <Link to="/signin" className="text-blue-500">Sign in</Link></p>
+                </div>
             </div>
-        </Box>
+        </div>
     )
 }
 
